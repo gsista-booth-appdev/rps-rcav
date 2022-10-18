bf97026319620c4bd7c8d517e840dba1861e13af
gsista-booth <gsista@chicagobooth.edu>
2022-10-18 05:42:01 +0000

    Progress on Paper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
-  layout(false)
+  layout("wrapper.html.erb")
 
   # Add your actions below this line
   # ================================
+
+  def homepage
+    render({ :template => "game_templates/rules.html.erb" })
+  end
 
   def play_rock
     #write a ton of code
@@ -10,7 +14,34 @@ class ApplicationController < ActionController::Base
     #redirect_to("https://www.wikipedia.org")
 
     # render({ :html => "<h1>Hello, World!</h1>".html_safe })
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    elsif @comp_move == "scissors"
+      @outcome = "won"
+    end
 
     render({ :template => "game_templates/user_rock.html.erb" })
+  end
+
+  def play_paper
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
+    end
+
+    render({ :template => "/game_templates/user_paper.html.erb" })
+  end
+
+  def play_scissors
+    render({ :template => "/game_templates/user_scissors.html.erb" })
   end
 end
